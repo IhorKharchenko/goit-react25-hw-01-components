8679c5c6938c5d8b979e29bd3ee3e4527a9a0755
Ihor Kharchenko <ihor.stavr@gmail.com>
2021-02-28 18:42:54 +0200

    composes used

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,13 +1,13 @@
-import React from "react";
-import styles from "./FriendList.module.css";
+import React from 'react';
+import styles from './FriendList.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={styles.item}>
       {isOnline ? (
-        <span className={styles.status + " " + styles.isOnline}></span>
+        <span className={styles.isOnline}></span>
       ) : (
-        <span className={styles.status + " " + styles.isOffline}></span>
+        <span className={styles.isOffline}></span>
       )}
       <img className={styles.avatar} src={avatar} alt={name} />
       <p className={styles.name}>{name}</p>
